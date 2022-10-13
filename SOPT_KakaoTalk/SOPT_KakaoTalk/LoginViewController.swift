0410b5c40ac7ccb4edf9d5d8d014c10f1c0e8ebe
Mini <minnnidev@gmail.com>
2022-10-13 16:14:02 +0900

    style: 로그인뷰 추가 (#1)

---
+++ SOPT_KakaoTalk/SOPT_KakaoTalk/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SOPT_KakaoTalk
+//
+//  Created by 김민 on 2022/10/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+    
+
+}
+
