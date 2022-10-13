3222af626968ac6d100ea110e39288b976a8dce6
Mini <minnnidev@gmail.com>
2022-10-13 16:22:53 +0900

    style: 웰컴뷰 UI 수정 (#1)

--- SOPT_KakaoTalk/SOPT_KakaoTalk/SceneDelegate.swift
+++ SOPT_KakaoTalk/SOPT_KakaoTalk/SceneDelegate.swift
@@ -24,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
             
             //let rootVC = LoginViewController()
-            let rootVC = SignupViewController()
+            let rootVC = WelcomeViewController()
             let navigationController = UINavigationController(rootViewController: rootVC)
             
             //window.rootViewController = navigationController
---
+++ SOPT_KakaoTalk/SOPT_KakaoTalk/WelcomeViewController.swift
@@ -0,0 +1,76 @@
+import UIKit
+import SnapKit
+
+//MARK: - WelcomeViewController
+class WelcomeViewController: UIViewController {
+    
+    //MARK: - UI Components
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = """
+                    님
+                    환영합니다
+                    """
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        
+        return label
+    }()
+    
+    private let completeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("확인", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15, weight: .medium)
+        button.backgroundColor = .systemYellow
+        button.layer.cornerRadius = 3
+        
+        return button
+    }()
+    
+    //MARK: - Properties
+    var email: String?
+
+    //MARK: - LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layout()
+        config()
+    }
+    
+    //MARK: - functions
+
+    
+    //MARK: - private functions
+
+    
+    //MARK: - objc functions
+}
+
+//MARK: - Extensions
+extension WelcomeViewController {
+    //MARK: - Layout
+    private func layout() {
+        view.addSubviews([welcomeLabel, completeButton])
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(197)
+            make.centerX.equalToSuperview()
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.top.equalTo(self.welcomeLabel.snp.bottom).offset(117)
+            make.leading.equalToSuperview().offset(21)
+            make.trailing.equalToSuperview().offset(-21)
+            make.height.equalTo(44)
+        }
+    }
+    
+    //MARK: - Config
+    private func config() {
+        view.backgroundColor = .white
+    }
+}
+
