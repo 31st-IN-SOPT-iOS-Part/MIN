0e2962dddbd704bb8d81d33e91aa23a900b0488c
Mini <minnnidev@gmail.com>
2022-10-18 23:13:36 +0900

    dir: 폴더 세팅

---
+++ Seminar/Week3_Seminar/Week3_Seminar/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week3_Seminar
+//
+//  Created by 김민 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
