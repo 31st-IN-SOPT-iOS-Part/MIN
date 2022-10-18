c419da2cfc7a73d34543399b90e452789dd68174
Mini <minnnidev@gmail.com>
2022-10-18 23:19:26 +0900

    feat: MusicModel 추가

---
+++ Seminar/Week3_Seminar/Week3_Seminar/Models/MusicModel.swift
@@ -0,0 +1,14 @@
+//
+//  MusicModel.swift
+//  Week3_Seminar
+//
+//  Created by 김민 on 2022/10/18.
+//
+
+import Foundation
+
+struct MusicModel {
+    let albumImage: String
+    let title: String
+    let singer: String
+}
--- Seminar/Week3_Seminar/Week3_Seminar/ViewControllers/MusicListViewController.swift
+++ Seminar/Week3_Seminar/Week3_Seminar/ViewControllers/MusicListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MusicListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
