cc443fdc1bd5d9e2a38e9e94bcf3a1cce90d859b
Mini <minnnidev@gmail.com>
2022-10-26 01:28:49 +0900

    style: 친구 뷰 프로필 UI 구현 (#6)

--- SOPT_KakaoTalk/SOPT_KakaoTalk/Sources/ViewControllers/Friend/FriendViewController.swift
+++ SOPT_KakaoTalk/SOPT_KakaoTalk/Sources/ViewControllers/Friend/FriendViewController.swift
@@ -19,7 +19,7 @@ class FriendViewController: UIViewController {
     }
     
     private lazy var friendTableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         
         tableView.backgroundColor = .clear
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -42,6 +42,10 @@ class FriendViewController: UIViewController {
         FriendModel(friendProfileImage: "profileImage7", friendName: "최솝트", friendMessage: "살려줘요"),
         FriendModel(friendProfileImage: "profileImage8", friendName: "고솝트", friendMessage: "살려줘요"),
         FriendModel(friendProfileImage: "profileImage9", friendName: "하솝트", friendMessage: "살려줘요"),
+        FriendModel(friendProfileImage: "profileImage6", friendName: "황솝트", friendMessage: "살려줘요"),
+        FriendModel(friendProfileImage: "profileImage7", friendName: "최솝트", friendMessage: "살려줘요"),
+        FriendModel(friendProfileImage: "profileImage8", friendName: "고솝트", friendMessage: "살려줘요"),
+        FriendModel(friendProfileImage: "profileImage9", friendName: "하솝트", friendMessage: "살려줘요"),
     ]
     
 
@@ -59,7 +63,6 @@ class FriendViewController: UIViewController {
     // MARK: - Properties
     
     var name: String?
-    
 
 }
 
@@ -93,12 +96,12 @@ extension FriendViewController {
             make.top.equalTo(titleView.snp.bottom)
             make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             make.bottom.equalToSuperview()
-            make.height.equalTo(50*friendList.count)
+            make.height.equalTo(50*friendList.count + 73)
         }
         
     }
     
-    // MARK: - Genral
+    // MARK: - General
     
     private func config() {
         view.backgroundColor = .white
@@ -107,10 +110,13 @@ extension FriendViewController {
     
     private func register() {
         friendTableView.register(FriendTableViewCell.self, forCellReuseIdentifier: FriendTableViewCell.identifier)
+        friendTableView.register(MyProfileHeaderView.self, forHeaderFooterViewReuseIdentifier: MyProfileHeaderView.identifer)
     }
 }
 
-extension FriendViewController: UITableViewDelegate, UITableViewDataSource {
+// MARK: - UITableViewDataSource
+
+extension FriendViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return friendList.count
     }
@@ -121,9 +127,22 @@ extension FriendViewController: UITableViewDelegate, UITableViewDataSource {
         cell.dataBind(dataModel: friendList[indexPath.row])
         return cell
     }
+}
+
+// MARK: - UITableViewDelegate
+
+extension FriendViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: MyProfileHeaderView.identifer) as? MyProfileHeaderView else {return UIView()}
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 73
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
-    
 }
