2193410356f1a956d64945b0f91d754644dba955
Mini <minnnidev@gmail.com>
2022-11-24 13:16:51 +0900

    [Setting] 프로젝트 생성 #10

---
+++ Seminar/Week4_Seminar/Week4_Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week4_Seminar
+//
+//  Created by 김민 on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
